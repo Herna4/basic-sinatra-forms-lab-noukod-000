f1f657ceb147890bc114e96068543d9c5b0cf2dd
Herna Michel <labellehernamichel@yahoo.fr>
2019-06-01 23:06:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,22 +1,15 @@
-require 'sinatra/base'
-
 class App < Sinatra::Base
 
-   get '/newteam' do
-     erb :newteam
-   end
-   
-   post '/team' do
-    @team= params["name"]
-    @coach = params["coach"]
-    @pg = params["pg"]
-    @sg = params["sg"]
-    @sf = params["sf"]
-    @pf = params["pf"]
-    @center = params["c"]
-    erb :team
-   end
-   
-   
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @team = params
+    erb :team
+  end
+
+
+end
 end
